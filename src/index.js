2b732a7765f45c84b9a94e428fea4432c9f66efd
Oleksandr Simakov <simakov.mailbox@gmail.com>
2021-05-12 21:46:26 +0300

    completes task1

--- src/index.js
+++ src/index.js
@@ -10,5 +10,39 @@ const colors = [
   '#009688',
   '#795548',
 ];
+const refs = {
+  start: document.querySelector('button[data-action="start"]'),
+  stop: document.querySelector('button[data-action="stop"]'),
+  bodyEl: document.querySelector('body'),
+};
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+const COLOR_CHANGE_DELAY = 1000;
+var intervalId = null;
 
-// code
+// element selection
+refs.start.addEventListener('click', onStartClick);
+refs.stop.addEventListener('click', onStopClick);
+
+// functions
+function onStartClick() {
+  disableStart();
+  intervalId = setInterval(() => {
+    console.log(randomIntegerFromInterval(0, colors.length));
+    refs.bodyEl.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length)];
+  }, COLOR_CHANGE_DELAY);
+}
+
+function disableStart() {
+  refs.start.setAttribute('disabled', true)
+}
+
+function onStopClick() {
+  enableStart();
+  clearInterval(intervalId);
+}
+
+function enableStart() {
+  refs.start.removeAttribute('disabled',true)
+}
